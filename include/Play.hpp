bd44fec0adac4b020e4cb40dd69a1e3616eda7d1
Must-ah <Mustafa.Abdel-Hussein@hotmail.com>
2022-06-08 19:14:49 +0200

    Crate Class skeleton

---
+++ include/Play.hpp
@@ -0,0 +1,21 @@
+#ifndef PLAY_HPP
+#define PLAY_HPP
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+
+class Pong;
+class Play
+{
+private:
+    /* data */
+
+public:
+    Play(/* args */);
+    ~Play();
+};
+
+
+
+
+#endif
---
+++ src/Play.cpp
@@ -0,0 +1,2 @@
+#include "../include/Play.hpp"
+
