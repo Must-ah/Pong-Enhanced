f94096a7d3c27f35a8b72c2219bbf7e21e32de7b
Must-ah <Mustafa.Abdel-Hussein@hotmail.com>
2022-06-10 14:02:07 +0200

    Create the Class and add methods
    I have created the Class and the Methods I am able to render the board and the Human player

--- include/Play.hpp
+++ include/Play.hpp
@@ -4,18 +4,33 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h>
 
-class Pong;
+#include <assert.h>
+
+
+#include "Pong.hpp"
+
 class Play
 {
 private:
     /* data */
+    SDL_Renderer *renderer;
+    SDL_Window *window;
+    TTF_Font *font;
+    SDL_Color color;
+    bool running;
+    int font_size;
+    
+    /* Methods*/
+    void init_SDL(Pong &pong);
+    void init_TTF(const int &size_font);
 
 public:
     Play(/* args */);
+    Play(Pong &pong);
     ~Play();
+
+    /* methods */
+    void render(Pong &pong);
 };
-
-
-
 
 #endif
--- src/Play.cpp
+++ src/Play.cpp
@@ -1,2 +1,55 @@
 #include "../include/Play.hpp"
 
+Play::Play(Pong &pong)
+{
+    init_SDL(pong);
+    font_size = 32;
+    init_TTF(font_size);
+    render(pong);
+}
+
+void Play::init_SDL(Pong &pong)
+{
+    assert(SDL_Init(SDL_INIT_EVERYTHING) >= 0);
+    auto const &[width, height] = pong.get_board_dimensions();
+    auto Create_Window_Render_Successfully = SDL_CreateWindowAndRenderer(width,
+                                                                         height,
+                                                                         SDL_WINDOW_SHOWN,
+                                                                         &window, &renderer);
+
+    assert(Create_Window_Render_Successfully >= 0);
+
+    SDL_SetWindowTitle(window, "Pong Enhanced");
+    SDL_ShowCursor(0);
+}
+
+void Play::init_TTF(const int &size_font)
+{
+    TTF_Init();
+    font = TTF_OpenFont("../include/Peepo.ttf", size_font);
+}
+
+void Play::render(Pong &pong)
+{
+    SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 255);
+    SDL_RenderClear(renderer);
+    SDL_Delay(5 / 60);
+    Player human = pong.get_human_player();
+    const auto &[width, height] = human.get_player_dimension();
+    SDL_Rect human_SDL;
+    human_SDL.h = height;
+    human_SDL.w = 12;
+    human_SDL.x = 32;
+    human_SDL.y = (900 / 2) - (human_SDL.h / 2);
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_RenderFillRect(renderer, &human_SDL);
+    SDL_RenderPresent(renderer);
+}
+
+Play::~Play()
+{
+    TTF_CloseFont(font);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
